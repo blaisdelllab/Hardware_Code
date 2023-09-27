bff3a29026ad5f1dc1b27b40a3735004d1a8d8a8
Blaisdell Lab <145798259+blaisdelllab@users.noreply.github.com>
2023-09-27 15:25:33 -0700

    Add files via upload

--- update_git.sh
+++ update_git.sh
@@ -17,25 +17,25 @@
 #chmod +x touchscreenMappingShellScriptName
 
 # P003e Repository
-cd Desktop/Experiments/P003e
+cd /home/blaisdelllab/Desktop/Experiments/P003e
 git reset --hard
 git pull origin main
 chmod +x P003E_ExpProgram_RP.py
 
 # P033 Repository
-cd Desktop/Experiments/P033
+cd /home/blaisdelllab/Desktop/Experiments/P033
 git reset --hard
 git pull origin main
 
 
 # P034 Repository
-cd Desktop/Experiments/P034
+cd /home/blaisdelllab/Desktop/Experiments/P034
 git reset --hard
 git pull origin main
 chmod +x P034_ExpProgram_RP.py
 
 # P035 Repository
-cd Desktop/Experiments/P035
+cd /home/blaisdelllab/Desktop/Experiments/P035
 git reset --hard
 git pull origin main
 chmod +x P035_FOAM_ExpProgram_RPi.py
