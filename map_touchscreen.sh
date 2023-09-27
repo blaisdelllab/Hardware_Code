a42868279a72074dd2882efbb0e69ea2d1e195d5
Blaisdell Lab <145798259+blaisdelllab@users.noreply.github.com>
2023-09-27 15:44:29 -0700

    Add files via upload

--- map_touchscreen.sh
+++ map_touchscreen.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-/usr/bin/xinput map-to-output 10 HDMI-2
+# Define the path to the text file
+text_file="/home/blaisdelllab/Desktop/Box_Info/Box_number.txt"
+
+# Read the value from the text file and assign it to a variable
+box_number=$(cat "$text_file")
+
+# Check if the variable is not empty (e.g., the file exists and contains a value)
+if [ -n "$box_number" ]; then
+    # Use the variable in the xinput command
+    /usr/bin/xinput map-to-output "$box_number" HDMI-2
+else
+    # Handle the case where the file is empty or does not exist
+    echo "Error: The text file is empty or does not exist."
+fi
