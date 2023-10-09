f11ce957a98406f495032afd6d21590a30742431
Blaisdell Lab <145798259+blaisdelllab@users.noreply.github.com>
2023-10-09 16:10:53 -0700

    Add files via upload

--- Hardware_test.py
+++ Hardware_test.py
@@ -12,7 +12,8 @@ beforehand, we can better look after the integrity of our experiments.
 Updates:
 - Ibrahim Y. on October 25, 2022
 - Rob T. on July 7, 2023
-- Cyrus K. for RPi/1024x768p on September 9, 2023 
+- Cyrus K. for RPi/1024x768p on September 9, 2023
+- Rob T., Cameron G., & Cyrus K. 2023-10-09 (spatial and quality of life updates)
 
 By Michael Nirula, Ibrahim Y., Cyrus K., Rob T.
 @author: michaelnirula
@@ -29,9 +30,13 @@ from gpiozero import Servo
 import pigpio
 import csv
 
-# First is declare whether we're running a version on operant boxes or our
-# individual machines
-operant_box_version = True
+# Decide if running a RPi version
+if os_path.expanduser('~').split("/")[2] =="blaisdelllab":
+    operant_box_version = True
+    print("*** Running operant box version *** \n")
+else:
+    operant_box_version = False
+    print("*** Running test version (no hardware) *** \n")
 
 root = Tk() #This creates the root to reference
 root.title("Test Hardware") # Creates the root title/title of the file
@@ -39,7 +44,7 @@ root.title("Test Hardware") # Creates the root title/title of the file
 # This creates the geometry of the gui, depending on version
 if operant_box_version:
     popen("sh /home/blaisdelllab/Desktop/Hardware_Code/map_touchscreen.sh") # Map touchscreen to operant box monitor
-    root.geometry("1024x768+1024+0") #  needs to be changed
+    root.geometry("1024x768+1024+0")
     root.attributes('-fullscreen',
                     True)
     canvas_1 = Canvas(root,
@@ -94,7 +99,7 @@ coordinate_list=[[150, 150, 225, 225], #TOP LEFT
                  [775, 350, 850, 425], #MIDDLE RIGHT
                  [150, 600, 225, 675], #BOTTOM LEFT
                  [475, 600, 550, 675], #BOTTOM MIDDLE
-                 [775, 600, 550, 675], #BOTTOM RIGHT
+                 [775, 600, 850, 675], #BOTTOM RIGHT
                  ]
 
 a=random.sample(coordinate_list,
@@ -116,27 +121,14 @@ shape_2 = canvas_1.create_rectangle(b,
                           outline="white",
                           tag="shape_2")
 
-#Links text to shape 1 using and if else statement. We have 9 total shapes in
-# the list with a total of 3 x coordinates. This will center the text above the
-# chosen x coordinate.
-if a[0][0]==100:
-    guidetext= canvas_1.create_text(a[0][0]+150,
-                                    a[0][1]-25,
-                                    text="Please click any shapes to test touchscreen :D",
-                                    font=("Helvetica", 20, "italic"),
-                                    fill="white",)
-elif a[0][0]==375:
-    guidetext= canvas_1.create_text(a[0][0]+40,
-                                    a[0][1]-25,
-                                    text="Please click any shapes to test touchscreen :D",
-                                    font=("Helvetica", 20, "italic"),
-                                    fill="white")
-else:
-    guidetext= canvas_1.create_text(a[0][0]-70,
-                                    a[0][1]-25,
-                                    text="Please click any shapes to test touchscreen :D",
-                                    font=("Helvetica", 24, "italic"),
-                                    fill="white",)
+# Standardizes "Please click" instruction to be at the center of the screen every time
+guidetext= canvas_1.create_text(512, 300,
+                                text="Please click any shapes to test touchscreen :D",
+                                font=("Helvetica", 20, "italic"),
+                                fill="white",
+                                justify='center'
+                                )
+
 
 touchscreenworks = canvas_1.create_text(512, 384,
                                         text="Touch-Screen Works!",
@@ -215,8 +207,10 @@ def rectangle2_pressed(*args):
                hopper_light_on)
 
 def hopper_light_on():
-    rpi_board.write(hopper_light_GPIO_num,
-                True)
+    print("Hopper light on")
+    if operant_box_version:
+        rpi_board.write(hopper_light_GPIO_num,
+                    True)
     canvas_1.itemconfigure(touchscreenworks,
                            state='hidden')
     canvas_1.itemconfigure(hopper_light_text_on,
@@ -225,8 +219,10 @@ def hopper_light_on():
                hopper_light_off)
 
 def hopper_light_off():
-    rpi_board.write(hopper_light_GPIO_num,
-                False)
+    print("Hopper light off")
+    if operant_box_version:
+        rpi_board.write(hopper_light_GPIO_num,
+                    False)
     canvas_1.itemconfigure(hopper_light_text_on,
                            state='hidden')
     canvas_1.itemconfigure(hopper_light_text_off,
@@ -235,8 +231,10 @@ def hopper_light_off():
                house_light_on)
 
 def house_light_on():
-    rpi_board.write(house_light_GPIO_num,
-                True)
+    print("House light on")
+    if operant_box_version:
+        rpi_board.write(house_light_GPIO_num,
+                    True)
     canvas_1.itemconfigure(hopper_light_text_off,
                            state='hidden')
     canvas_1.itemconfigure(house_light_text_on,
@@ -245,8 +243,10 @@ def house_light_on():
                house_light_off)
 
 def house_light_off():
-    rpi_board.write(house_light_GPIO_num,
-                False)
+    print("House light off")
+    if operant_box_version:
+        rpi_board.write(house_light_GPIO_num,
+                    False)
     canvas_1.itemconfigure(house_light_text_on,
                            state='hidden')
     canvas_1.itemconfigure(house_light_text_off,
@@ -255,26 +255,34 @@ def house_light_off():
                hopper_on)
 
 def hopper_on():
+    print("Hopper up")
+    if operant_box_version:
+        rpi_board.set_servo_pulsewidth(servo_GPIO_num, hopper_up_val)
     canvas_1.itemconfigure(house_light_text_off,
                            state='hidden')
     canvas_1.itemconfigure(hopper_text_on,
                            state='normal')
-    rpi_board.set_servo_pulsewidth(servo_GPIO_num, hopper_up_val)
     root.after(3000,
                hopper_off)
 
 def hopper_off():
+    print("Hopper down")
+    if operant_box_version:
+        rpi_board.set_servo_pulsewidth(servo_GPIO_num, hopper_down_val)
     canvas_1.itemconfigure(hopper_text_on,
                            state='hidden')
     canvas_1.itemconfigure(hopper_text_off,
                            state='normal')
-    rpi_board.set_servo_pulsewidth(servo_GPIO_num, hopper_down_val)
     root.after(1000,
                all_systems_go)
 
 def all_systems_go():
-    rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
-    rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
+    print("All systems go! :D")
+    if random.random() < 0.01:
+        print("- YOU HAVE BEEN CHOSEN AS THE NEXT LEAD RA. CONTACT AUTHORITIES IMMEDIATELY.")
+    if operant_box_version:
+        rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
+        rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
     canvas_1.itemconfigure(hopper_text_off,
                            state='hidden')
     canvas_1.itemconfigure(success_text,
@@ -297,9 +305,10 @@ def start_countdown():
                    start_countdown)
     else:
         root.destroy()
-        rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
-        rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
-        rpi_board.stop()
+        if operant_box_version:
+            rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
+            rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
+            rpi_board.stop()
 
 
 
@@ -316,6 +325,7 @@ canvas_1.tag_bind("shape_2",
 try:
     root.mainloop()
 except:
-    rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
-    rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
-    rpi_board.stop()
+    if operant_box_version:
+        rpi_board.set_PWM_dutycycle(servo_GPIO_num, 0)
+        rpi_board.set_PWM_frequency(servo_GPIO_num, 0)
+        rpi_board.stop()
