1918022d1d2c84b0b0062fc6605e84a85b1fd3c9
Blaisdell Lab <145798259+blaisdelllab@users.noreply.github.com>
2025-03-13 07:43:33 -0700

    Update update_git.sh

--- update_git.sh
+++ update_git.sh
@@ -9,6 +9,9 @@
 # Last updated: 2025-03-06
 # Authors: Cyrus K.
 
+# Run the entire script with lower CPU and I/O priority
+exec nice -n 10 ionice -c 3 bash -c '
+
 # Function to update a repository if it exists
 for repo in \
     "/home/blaisdelllab/Desktop/Hardware_Code" \
